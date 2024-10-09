184e4dbe74992b8761d226ec67963072828fe6de
happycozero <fermer141@yandex.ru>
2024-10-09 17:44:15 +0300

    Улучшен и упрощен код

--- Python/main.py
+++ Python/main.py
@@ -9,57 +9,57 @@ token = input("Введите токен: ")
 vk_session = vk_api.VkApi(token=token)
 vk = vk_session.get_api()
 
-# Открытие файлов с html-кодом
+# Открытие файла с html-кодом и чтение содержимого
 with open(f"{path}/photo_pre.html", "r", encoding="utf8") as f:
-    # Чтение содержимого всех трех файлов в отдельные переменные
-    file, file1, file2 = f.read(), f.read(), f.read()
+    file, file1, file2 = [f.read() for _ in range(3)]
 
 try:
     # Получение информации о профиле пользователя
-    getinfo = vk.account.getProfileInfo()
-    iddd = getinfo["id"]
-    vk_name = getinfo["first_name"]
-    vk_rename = getinfo["last_name"]
+    profile_info = vk.account.getProfileInfo()
+    user_id = profile_info["id"]
+    first_name = profile_info["first_name"]
+    last_name = profile_info["last_name"]
 
     # Получение списка диалогов пользователя
-    test = vk.messages.getConversations(count=200)
-    num = test["count"]
-    print(f"Всего найдено диалогов: {num}")
-    print(f"Начинаю выгрузку фотографий | {vk_name} {vk_rename} - vk.com/id{iddd}")
+    conversations = vk.messages.getConversations(count=200)
+    print(f"Всего найдено диалогов: {conversations['count']}")
+    print(f"Начинаю выгрузку фотографий | {first_name} {last_name} - vk.com/id{user_id}")
 
     # Обработка каждого диалога
-    for i in test["items"]:
-        idd = i["conversation"]["peer"]["id"]
-        peer_type = i["conversation"]["peer"]["type"]
+    for conversation in conversations["items"]:
+        peer_id = conversation["conversation"]["peer"]["id"]
+        peer_type = conversation["conversation"]["peer"]["type"]
 
-        if peer_type == "user" and idd > 0: # Обработка диалогов с пользователями
-            print(f"Выгрузка фотографий - {idd}")
-            testtt = vk.users.get(user_ids=idd, fields="sex")
+        if peer_type == "user" and peer_id > 0:  # Только диалоги с пользователями
+            print(f"Выгрузка фотографий - {peer_id}")
+            user_info = vk.users.get(user_ids=peer_id, fields="sex")[0]
+            sex = user_info["sex"]
 
-            for b in testtt:
-                pol_ebaniy = b["sex"]
-                file_to_write = file if pol_ebaniy == 1 else file1 if pol_ebaniy == 2 else file2
-                fo = vk.messages.getHistoryAttachments(peer_id=idd, media_type="photo", start_from=0, count=200, preserve_order=1, max_forwards_level=45)
+            file_to_write = {1: file, 2: file1}.get(sex, file2)
+            attachments = vk.messages.getHistoryAttachments(
+                peer_id=peer_id, media_type="photo", count=200, preserve_order=1, max_forwards_level=45
+            )
 
-                # Обработка каждой фотографии в диалоге
-                for i in fo["items"]:
-                    for j in i["attachment"]["photo"]["sizes"]:
-                        if 500 < j["height"] < 650:
-                            url = j["url"]
-                            file_to_write += f'<img class="photos" src="{url}" alt="Не удалось загрузить (:" title="Найдено в диалоге - vk.com/id{idd}">'
+            # Обработка фотографий
+            for attachment in attachments["items"]:
+                for size in attachment["attachment"]["photo"]["sizes"]:
+                    if 500 < size["height"] < 650:
+                        url = size["url"]
+                        file_to_write += f'<img class="photos" src="{url}" alt="Не удалось загрузить (:" title="Найдено в диалоге - vk.com/id{peer_id}">'
 
-        elif peer_type == "group": # Обработка диалогов с группами
-            print("Это группа!")
-        else: # Обработка диалогов в конференциях
-            print("Это конфа!")
+        else:
+            print("Это группа!" if peer_type == "group" else "Это конфа!")
 
     # Запись результатов в файлы
-    with open(f"{path}/Девочки - id{iddd}.html", "w+", encoding="utf8") as f:
-        f.write(file)
-    with open(f"{path}/Мальчики - id{iddd}.html", "w+", encoding="utf8") as f:
-        f.write(file1)
-    with open(f"{path}/Не определено - id{iddd}.html",  "w+", encoding="utf8") as f:
-        f.write(file2)
+    output_files = {
+        f"{path}/Девочки - id{user_id}.html": file,
+        f"{path}/Мальчики - id{user_id}.html": file1,
+        f"{path}/Не определено - id{user_id}.html": file2,
+    }
+
+    for file_path, content in output_files.items():
+        with open(file_path, "w+", encoding="utf8") as f:
+            f.write(content)
 
 except Exception as e:
-    print(e)
+    print(f"Произошла ошибка: {e}")
